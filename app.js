1d18b3e61c765863675f50278828484526576bd5
Gabriel Henrique <54560401+GroundWave96@users.noreply.github.com>
2023-11-15 22:47:40 -0300

    Update project

--- app.js
+++ app.js
@@ -28,3 +28,59 @@ var swiper = new Swiper(".products", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".productsMobile", {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/* var swiper = new Swiper(".productsMobile", {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+}); */
+
+/* window.addEventListener('resize', function () {
+  if (window.matchMedia('(max-width: 1250px)').matches) {
+    var swiper = new Swiper(".products", {
+      slidesPerView: 2,
+      spaceBetween: 0,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } else {
+    var swiper = new Swiper(".products", {
+      slidesPerView: 4,
+      spaceBetween: 0,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+}); */
